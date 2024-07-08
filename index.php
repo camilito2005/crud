e2816c0dbf750b1cb7e5eae82312b8b11e2d051b
camilito2005 <134243118+camilito2005@users.noreply.github.com>
2024-07-08 15:07:20 -0500

    Add files via upload

--- index.php
+++ index.php
@@ -2,17 +2,25 @@
 <html lang="en">
 
 <head>
-    <link rel="stylesheet" href="./css/formulario2.css">
+<script
+  src="https://code.jquery.com/jquery-3.7.1.min.js"
+  integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo="
+  crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="./css/formulario3.css">
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
 
 <body>
+    <nav class="navv">
+        <form class="formu-buscar">
+            <input class="buscar" type="search" id="search" placeholder="search">
+            <input class="enviar" type="submit" value="search">
+        </form>
+    </nav>
     <h2 class="titulo">index</h2>
-<form action="">
-    <input type="search" placeholder="...">
-</form>
+
     <?php
 
     include "./articulos/lib_articulos.php";
